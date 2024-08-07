707e9ed7fcbcee49422921635fdefd5de5245be8
Lananh Nguyen <lananhnguyen@live.co.uk>
2024-08-07 22:16:48 +0100

    text added to onboarding carousel

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Speedrail"
+  config.site_title = "CommonSpace Admin Board"
 
   # == Load Paths
   #
