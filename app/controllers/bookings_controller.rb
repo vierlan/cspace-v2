0a2f38beceb88a0d3a8adefa1f86bc638fbbfd5b
Lananh Nguyen <lananhnguyen@live.co.uk>
2024-08-25 15:50:29 +0100

    checkout session implented after booking create with automated redirect.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,4 @@
 class BookingsController < ApplicationController
-  before_action :authenticate_user!
-
   def new
     @booking = Booking.new
     @booking.user = current_user
@@ -10,28 +8,72 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(booking_params)
     @booking.user = current_user
-    @booking.package = Package.find(params[:package_id])
-    @booking.venue = @booking.package.venue
+    @package = Package.find(params[:package_id])
+    @venue = @package.venue
+    @booking.package = @package
+    @booking.venue = @venue
+    @stripe_price_id = @package.stripe_price_id
+    @booking_date = params[:booking][:booking_date]
+    @booking_start_time = params[:booking][:booking_start_time]
 
     if @booking.save
-      Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-      current_user.set_payment_processor :stripe
-      current_user.payment_processor.customer
-      @checkout_session = current_user
-                          .payment_processor
-                          .checkout(
-                            mode: 'payment',
-                            line_items: 'price_1PpvXoRszoiavRvriwLx0Fqy',
-                            success_url: checkout_success_url,
-                            cancel_url: checkout_cancel_url,
-                          )
+      @booking_id = @booking.id
+      Rails.logger.info "Booking created with ID: #{@booking_id}, booking date: #{@booking_date}, booking start time: #{@booking_start_time}"
+      @checkout_session = create_stripe_checkout_session(@booking)
+      redirect_to @checkout_session.url, status: 303, allow_other_host: true
+      Rails.logger.info "Redirecting to Stripe checkout session URL: #{@checkout_session.url}"
+
+     #respond_to do |format|
+     #  format.html { redirect_to @checkout_session.url, allow_other_host: true }
+     #  format.turbo_stream { render turbo_stream: turbo_stream.replace("checkout_redirect", partial: "shared/redirect", locals: { url: @checkout_session.url }) }
+     #end
     else
-      render :new
+      Rails.logger.error "Booking save failed: #{@booking.errors.full_messages.join(', ')}"
+      flash.now[:error] = 'Something went wrong'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def create_stripe_checkout_session(booking)
+    Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+    @booking = booking
+    @booking_id = @booking.id
+    @booking_start_time = @booking.booking_start_time
+    @booking_date = @booking.booking_date
+    @package = Package.find(params[:package_id])
+    @venue = @package.venue
+    @stripe_price_id = @package.stripe_price_id
+    current_user.set_payment_processor :stripe
+    current_user.payment_processor.customer
+    Rails.logger.info "Creating Stripe checkout session with price ID: #{@stripe_price_id}, booking ID: #{@booking_id}, booking date: #{@booking_date}, booking start time: #{@booking_start_time}"
+    begin
+      @checkout_session = current_user
+                            .payment_processor
+                            .checkout(
+                              mode: 'payment',
+                              line_items: [{
+                                price: @stripe_price_id,
+                                quantity: 1
+                              }],
+                              metadata: {
+                                venue_id: @venue.id,
+                                package_id: @package.id,
+                                booking_date: @booking_date,
+                                booking_start_time: @booking_start_time,
+                                booking_id: @booking_id
+                              },
+                              success_url: checkout_success_url,
+                              cancel_url: checkout_cancel_url
+                            )
+    rescue Stripe::InvalidRequestError => e
+      Rails.logger.error "Stripe error: #{e.message}"
+      flash[:error] = "There was a problem creating the Stripe checkout session: #{e.message}"
+      redirect_to new_booking_path
     end
   end
 
   def index
-    if current_user.venue_ower?
+    if current_user.venue_owner?
       @bookings = Booking.where(venue_id: current_user.venue.id)
     else
       @bookings = Booking.where(user_id: current_user.id)
@@ -64,6 +106,6 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit(:booking_date, :booking_start_time, :package_id)
+    params.require(:booking).permit(:booking_date, :booking_start_time)
   end
 end
